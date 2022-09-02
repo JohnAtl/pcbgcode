0c0134acde1711536cddcec27b3eb7b78cad1169
Johnson, John T <john.johnson@gatech.edu>
2022-09-02 10:52:18 -0400

    Arc center was being computed incorrectly after switching to IJ coordinates. Fixed.

--- source/pcb-gcode.h
+++ source/pcb-gcode.h
@@ -47,12 +47,13 @@ enum { false = 0, true = 1 };
 enum { TASK_INVALID, TASK_OUTLINES, TASK_FILL };
 
 enum { ST_INVALID,
-       ST_START_LINE, ST_CONTINUE_LINE, ST_END_LINE,
-       ST_DRILL,
-       ST_FILL,
-		 ST_ARC_BEGIN,
-		 ST_ARC_END,
-		 ST_ARC};
+    ST_START_LINE, ST_CONTINUE_LINE, ST_END_LINE,
+    ST_DRILL,
+    ST_FILL,
+    ST_ARC_BEGIN,
+    ST_ARC_END,
+    ST_ARC,
+    ST_ARC_CENTER};
 
 enum { TOP = 0, BOTTOM = 1, MILL = 2, TEXT = 3, STENCIL = 4, ALL = 5 };
 
